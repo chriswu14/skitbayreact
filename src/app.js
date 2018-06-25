d86bb2d717109bba060c9a3dc56c44245eac0dea
Chris Wu <Chriswu@Lijias-MacBook-Pro.local>
2018-06-26 02:03:27 +1000

    add material-ui, styled components configuring some settings

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+
 import store from './features/store';
+import App from './components/App'
 
 ReactDOM.render(
-    <Provider store={store}>
-       <div>doGood</div>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
 );
