e8694b7df5b42c79da6fd36f900eeeb90722b751
Chris Wu <Chriswu@Lijias-MacBook-Pro.local>
2018-07-07 23:44:59 +1000

    refactor landing component template with styled component

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -7,17 +7,17 @@ import BodyHeartImg from '../../images/heart-img.svg'
 export default class Landing extends Component {
   render() {
     return (
-      <GradientSection className={`hero is-large ${this.props.className}`}>
-        <HeartBackgroundDiv className={`hero-body ${this.props.className}`}>
+      <Hero>
+        <HeroBody>
           <div className="container">
             <div className=" columns is-gapless">
                 <div className="column">
-                  <h2 className="subtitle is-2" style={{marginBottom: '3rem', fontWeight: 'normal'}}>
+                  <TopSectionH2>
                     Connect, organise teams and raise funds transparently
-                  </h2>
-                  <h1 className="title is-1" style={{marginBottom: '3rem', fontSize: '5rem'}}>
+                  </TopSectionH2>
+                  <TopSectionH1>
                     <LogoText/>
-                  </h1>
+                  </TopSectionH1>
                   <p>
                     doGood is a Charity Social Network built on top of STEEM blockchain which pays doGooders for organizing, volunteering and voting for charitable actions
                   </p>
@@ -27,13 +27,15 @@ export default class Landing extends Component {
                 </div>
               </div>
           </div>
-        </HeartBackgroundDiv>
-      </GradientSection>
+        </HeroBody>
+      </Hero>
     );
   }
 }
 
-const GradientSection = styled.section`
+const Hero = styled.section.attrs({
+  className: 'hero is-large'
+})`
   background: #d91839;  /* fallback for old browsers */
   background: -webkit-linear-gradient(301deg, #d91839, #4f136d);  /* Chrome 10-25, Safari 5.1-6 */
   background: linear-gradient(301deg, #d91839, #4f136d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
@@ -43,11 +45,31 @@ const GradientSection = styled.section`
   }
 `;
 
-const HeartBackgroundDiv = styled.div`
+const HeroBody = styled.div.attrs({
+  className: 'hero-body'
+})`
   @media(min-width: 769px) {
     background: url(${BodyHeartImg});
     background-repeat: no-repeat;
     background-position: 100% 50%;
     background-size: 50%;
+  }
+`;
+
+const TopSectionH1 = styled.h1.attrs({
+  className: 'title is-1'
+})`
+  &&& {
+    margin-bottom: 2.5rem;
+    font-size: 5rem;
+  }
+`;
+
+const TopSectionH2 = styled.h2.attrs({
+  className: 'subtitle is-2'
+})`
+  &&& {
+    margin-bottom: 3rem;
+    font-weight: normal;
   }
 `;
