6b8a5e85bf2fb2ee4f1d016e6fd5c5ab77c15474
Synclicity <tony.xu1@hotmail.com>
2018-08-06 23:41:33 +1000

    Add Tony's photo

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,156 +1,157 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
 import RectangleGradient from "../sharedComponents/RectangleGradient";
 import SectionHeading from "../sharedComponents/SectionHeading";
 import DescriptionText from "../sharedComponents/DescriptionText";
 
-import AndrasHajgatoImg from '../../images/team/AndrasHajgato.jpg';
-import CalebJiuImg from '../../images/team/CalebJiu.jpg';
-import CecileDingImg from '../../images/team/CecileDing.jpg';
-import ChrisWuImg from '../../images/team/ChrisWu.jpg';
-import DanniLiuImg from '../../images/team/DanniLiu.jpg';
-import EkitchoKeoImg from '../../images/team/EkitchoKeo.jpg';
-import TianHaoLiuImg from '../../images/team/TianHaoLiu.jpg';
-import VictorDoanImg from '../../images/team/VictorDoan.jpg';
-
+import AndrasHajgatoImg from "../../images/team/AndrasHajgato.jpg";
+import CalebJiuImg from "../../images/team/CalebJiu.jpg";
+import CecileDingImg from "../../images/team/CecileDing.jpg";
+import ChrisWuImg from "../../images/team/ChrisWu.jpg";
+import DanniLiuImg from "../../images/team/DanniLiu.jpg";
+import EkitchoKeoImg from "../../images/team/EkitchoKeo.jpg";
+import TianHaoLiuImg from "../../images/team/TianHaoLiu.jpg";
+import VictorDoanImg from "../../images/team/VictorDoan.jpg";
+import TonyXuImg from "../../images/team/TonyXu.jpg";
 
 export default class Landing extends Component {
-  constructor() {
-    super();
-    this.state = {
-        teamMembers: [
-          {
-            imgUrl: EkitchoKeoImg,
-            fullName: 'Ekitcho Keo',
-            teamPosition: 'Advisor'
-          },
-          {
-            imgUrl: TianHaoLiuImg,
-            fullName: 'Tian Hao Liu',
-            teamPosition: 'Advisor'
-          },          
-          {
-            imgUrl: VictorDoanImg,
-            fullName: 'Victor Doan',
-            teamPosition: 'Founder and CEO'
-          },
-          {
-            imgUrl: DanniLiuImg,
-            fullName: 'Danni Liu',
-            teamPosition: 'Co-founder and COO'
-          },        
-          {
-            imgUrl: ChrisWuImg,
-            fullName: 'Chris Wu',
-            teamPosition: 'Co-founder and Wizard'
-          },
-          {
-            imgUrl: AndrasHajgatoImg,
-            fullName: 'Andras Hajgato',
-            teamPosition: 'Co-founder and Designer'
-          },
-          {
-            imgUrl: 'https://bulma.io/images/placeholders/128x128.png',
-            fullName: 'Tony Xu',
-            teamPosition: 'Co-founder and Dev Weapon'
-          },
-          {
-            imgUrl: CalebJiuImg,
-            fullName: 'Caleb Jiu',
-            teamPosition: 'Assistant Director'
-          },
-          {
-            imgUrl: CecileDingImg,
-            fullName: 'Cecile Ding',
-            teamPosition: 'Marketplace Analyst'
-          }          
-        ],       
-    };   
-  }
-  render() {
-    return (
-      <Hero>
-        <HeroBody>
-          <div className="container">
-            <div className="columns is-gapless is-desktop">
-              <div className="column">
-                <SectionHeading>team</SectionHeading>
-                <DescriptionText>
-                  With decades of experience our team is comprised of people from around the world. All focused on one goal, building good Karma.
-                </DescriptionText>
-                <RectangleGradient />
-              </div>
-              <div className="column" />
-            </div>
-            <div className="columns is-multiline is-mobile is-gapless">
-              {
-                this.state.teamMembers.map((value, index) => {                           
-                  return (
-                    <div key={index} className="column is-half-mobile is-one-third-tablet is-one-quarter-desktop">  
-                      <TeamCard {...value}/>        
+    constructor() {
+        super();
+        this.state = {
+            teamMembers: [
+                {
+                    imgUrl: EkitchoKeoImg,
+                    fullName: "Ekitcho Keo",
+                    teamPosition: "Advisor"
+                },
+                {
+                    imgUrl: TianHaoLiuImg,
+                    fullName: "Tian Hao Liu",
+                    teamPosition: "Advisor"
+                },
+                {
+                    imgUrl: VictorDoanImg,
+                    fullName: "Victor Doan",
+                    teamPosition: "Founder and CEO"
+                },
+                {
+                    imgUrl: DanniLiuImg,
+                    fullName: "Danni Liu",
+                    teamPosition: "Co-founder and COO"
+                },
+                {
+                    imgUrl: ChrisWuImg,
+                    fullName: "Chris Wu",
+                    teamPosition: "Co-founder and Wizard"
+                },
+                {
+                    imgUrl: AndrasHajgatoImg,
+                    fullName: "Andras Hajgato",
+                    teamPosition: "Co-founder and Designer"
+                },
+                {
+                    imgUrl: TonyXuImg,
+                    fullName: "Tony Xu",
+                    teamPosition: "Co-founder and Dev Weapon"
+                },
+                {
+                    imgUrl: CalebJiuImg,
+                    fullName: "Caleb Jiu",
+                    teamPosition: "Assistant Director"
+                },
+                {
+                    imgUrl: CecileDingImg,
+                    fullName: "Cecile Ding",
+                    teamPosition: "Marketplace Analyst"
+                }
+            ]
+        };
+    }
+    render() {
+        return (
+            <Hero>
+                <HeroBody>
+                    <div className="container">
+                        <div className="columns is-gapless is-desktop">
+                            <div className="column">
+                                <SectionHeading>team</SectionHeading>
+                                <DescriptionText>
+                                    With decades of experience our team is
+                                    comprised of people from around the world.
+                                    All focused on one goal, building good
+                                    Karma.
+                                </DescriptionText>
+                                <RectangleGradient />
+                            </div>
+                            <div className="column" />
+                        </div>
+                        <div className="columns is-multiline is-mobile is-gapless">
+                            {this.state.teamMembers.map((value, index) => {
+                                return (
+                                    <div
+                                        key={index}
+                                        className="column is-half-mobile is-one-third-tablet is-one-quarter-desktop"
+                                    >
+                                        <TeamCard {...value} />
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
-                  );
-                })            
-              }              
-            </div>
-          </div>
-        </HeroBody>
-      </Hero>
-    );
-  }
+                </HeroBody>
+            </Hero>
+        );
+    }
 }
 
 const Hero = styled.section.attrs({
-  className: 'hero',
-  id: 'team'
-})`    
-`;
+    className: "hero",
+    id: "team"
+})``;
 
 const HeroBody = styled.div.attrs({
-  className: 'hero-body'
+    className: "hero-body"
 })`
-  @media (min-width: 769px) {
-    padding-top: 4rem;
-  }
+    @media (min-width: 769px) {
+        padding-top: 4rem;
+    }
 `;
 
 const TeamCard = props => {
-  return (
-    <TeamCardContainer>              
-      <figure className="image is-inline-block is-128x128">
-        <img className="is-rounded" src={props.imgUrl}/>
-      </figure>                
-      <TeamCardName>{props.fullName}</TeamCardName>
-      <TeamCardPosition>{props.teamPosition}</TeamCardPosition>  
-    </TeamCardContainer>  
-  );
+    return (
+        <TeamCardContainer>
+            <figure className="image is-inline-block is-128x128">
+                <img className="is-rounded" src={props.imgUrl} />
+            </figure>
+            <TeamCardName>{props.fullName}</TeamCardName>
+            <TeamCardPosition>{props.teamPosition}</TeamCardPosition>
+        </TeamCardContainer>
+    );
 };
 
 const TeamCardContainer = styled.div.attrs({
-  className: 'has-text-centered'
+    className: "has-text-centered"
 })`
-  padding-top: 3rem;
-  padding-bottom: 3rem;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
 `;
 
 const TeamCardName = styled.div.attrs({
-  className: 'is-uppercase'
+    className: "is-uppercase"
 })`
-  font-size: 1.25rem;
-  color: #154574;
-  font-weight: bold;
-  padding-top: 1rem;
-  padding-bottom: 0.3rem;
-  letter-spacing: 0.05rem;
+    font-size: 1.25rem;
+    color: #154574;
+    font-weight: bold;
+    padding-top: 1rem;
+    padding-bottom: 0.3rem;
+    letter-spacing: 0.05rem;
 `;
 
 const TeamCardPosition = styled.div`
-  padding-top: 0.3rem;
-  padding-bottom: 0.3rem;
-  color: #238fad;  
-  font-size: 1rem;
-  letter-spacing: 0.06rem;
+    padding-top: 0.3rem;
+    padding-bottom: 0.3rem;
+    color: #238fad;
+    font-size: 1rem;
+    letter-spacing: 0.06rem;
 `;
-
-
