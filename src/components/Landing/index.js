250c80524f3fe70ab6b90be96dd51d221f0a1a01
Synclicity <tony.xu1@hotmail.com>
2018-08-17 22:40:36 +1000

    Add whitepaper download links

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,89 +1,123 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import LogoText from '../sharedComponents/LogoText'
-import BodyImage from '../../images/skitbay_intro.png'
+import LogoText from "../sharedComponents/LogoText";
+import BodyImage from "../../images/skitbay_intro.png";
 
 export default class Landing extends Component {
-  render() {
-    return (
-      <Hero>
-        <HeroBody>
-          <div className="container">
-            <div className="columns is-gapless">
-                <div className="column">
-                  <TopSectionH2>
-                    Shop within the new economy.  
-                  </TopSectionH2>
-                  <TopSectionH1>
-                    <LogoText/>
-                  </TopSectionH1>
-                  <DescriptionSubText>
-                    <p>
-                      Skitbay combines marketplace, social network and blockchain to bring to you the next generation shopping experience. Shop through hundreds of stores and thousands of products, pay using cryptocurrencies, and get financially rewarded with the native $SKT token for posting, sharing or rating products.
-                    </p>
-                  </DescriptionSubText>
-                </div>
-                <div className="column" >
-
-                </div>
-              </div>
-          </div>
-        </HeroBody>
-      </Hero>
-    );
-  }
+    render() {
+        return (
+            <Hero>
+                <HeroBody>
+                    <div className="container">
+                        <div className="columns is-gapless">
+                            <div className="column">
+                                <TopSectionH2>
+                                    Shop within the new economy.
+                                </TopSectionH2>
+                                <TopSectionH1>
+                                    <LogoText />
+                                </TopSectionH1>
+                                <DescriptionSubText>
+                                    <p>
+                                        Skitbay combines marketplace, social
+                                        network and blockchain to bring to you
+                                        the next generation shopping experience.
+                                        Shop through hundreds of stores and
+                                        thousands of products, pay using
+                                        cryptocurrencies, and get financially
+                                        rewarded with the native $SKT token for
+                                        posting, sharing or rating products.
+                                    </p>
+                                </DescriptionSubText>
+                                <BottomSection>
+                                    <LinkButton link="https://s3.amazonaws.com/skitbay-app/Skitbay_whitepaper_beta_1.0.pdf">
+                                        Download Whitepaper
+                                    </LinkButton>
+                                </BottomSection>
+                            </div>
+                            <div className="column" />
+                        </div>
+                    </div>
+                </HeroBody>
+            </Hero>
+        );
+    }
 }
 
 const Hero = styled.section.attrs({
-  className: 'hero'
-})`  
-  background: -webkit-linear-gradient(306deg, #29e0ba, #2387ac 34%, #133c6d);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(306deg, #29e0ba, #2387ac 34%, #133c6d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    className: "hero"
+})`
+    background: -webkit-linear-gradient(
+        306deg,
+        #29e0ba,
+        #2387ac 34%,
+        #133c6d
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+        306deg,
+        #29e0ba,
+        #2387ac 34%,
+        #133c6d
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-  * {
-    color: white;
-  }
+    * {
+        color: white;
+    }
 `;
 
 const HeroBody = styled.div.attrs({
-  className: 'hero-body'
+    className: "hero-body"
 })`
-  padding-top: 8rem;
-  padding-bottom: 8rem;
+    padding-top: 8rem;
+    padding-bottom: 8rem;
 
-  @media(min-width: 769px) {
-    background: url(${BodyImage});
-    background-repeat: no-repeat;
-    background-position: 100% 100%;
-    background-size: auto 85%;
-  }
+    @media (min-width: 769px) {
+        background: url(${BodyImage});
+        background-repeat: no-repeat;
+        background-position: 100% 100%;
+        background-size: auto 85%;
+    }
 `;
 
 const TopSectionH1 = styled.h1.attrs({
-  className: 'title is-1'
+    className: "title is-1"
 })`
-  &&& {
-    margin-bottom: 2.5rem;
-    font-size: 5rem;
-  }
+    &&& {
+        margin-bottom: 2.5rem;
+        font-size: 5rem;
+    }
 `;
 
 const TopSectionH2 = styled.h2.attrs({
-  className: 'subtitle is-2'
+    className: "subtitle is-2"
 })`
-  &&& {
-    margin-bottom: 3rem;
-    font-weight: normal;
-  }
+    &&& {
+        margin-bottom: 3rem;
+        font-weight: normal;
+    }
+`;
+
+const BottomSection = styled.div`
+    margin-top: 2rem;
+`;
+
+const LinkButton = styled.a.attrs({
+    className: "button is-rounded is-info",
+    target: props => (props.link ? "_blank" : undefined),
+    href: props => (props.link ? props.link : undefined)
+})`
+    &&& {
+        font-size: 1.5rem;
+        font-weight: bold;
+        padding: 1rem 2rem;
+    }
 `;
 
 const DescriptionSubText = styled.div`
-  {     
-      font-size: 18px;
-      font-weight: 300;
-      font-style: normal;
-      font-stretch: normal;
-      line-height: 40px;       
-  }
+    font-size: 18px;
+    font-weight: 300;
+    font-style: normal;
+    font-stretch: normal;
+    line-height: 40px;
 `;
--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -1,50 +1,77 @@
-import React, { Component } from 'react';
-import LogoText from '../sharedComponents/LogoText'
+import React, { Component } from "react";
+import LogoText from "../sharedComponents/LogoText";
 
 export default class TopNav extends Component {
-  constructor(props){
-      super(props);
-      this.state = { showBurgerMenu: false }
-      this.toggleBurger = this.toggleBurger.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { showBurgerMenu: false };
+        this.toggleBurger = this.toggleBurger.bind(this);
+    }
 
-  toggleBurger() {
-    this.setState({ showBurgerMenu: !this.state.showBurgerMenu });
-  }
+    toggleBurger() {
+        this.setState({ showBurgerMenu: !this.state.showBurgerMenu });
+    }
 
-  render() {
-    let toggleClass = this.state.showBurgerMenu ? "is-active" : "";
-    return (
-      <nav className="navbar is-fixed-top">
-        <div className="container">
-          <div className="navbar-brand">
-            <a className="navbar-item" href="/">
-              <LogoText badge="Beta"/>
-            </a>
-            <div className={"navbar-burger burger " + toggleClass} onClick={this.toggleBurger}>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </div>
-          <div className={"navbar-menu " + toggleClass}>
-            <div className="navbar-end">
-              <a className="navbar-item" href="#about" onClick={this.toggleBurger}>
-                ABOUT
-              </a>
-              <a className="navbar-item" href="#roadMap" onClick={this.toggleBurger}>
-                ROADMAP
-              </a>             
-              <a className="navbar-item" href="#team" onClick={this.toggleBurger}>
-                TEAM
-              </a>            
-              <a className="navbar-item" href="#faq" onClick={this.toggleBurger}>
-                FAQ
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+    render() {
+        let toggleClass = this.state.showBurgerMenu ? "is-active" : "";
+        return (
+            <nav className="navbar is-fixed-top">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="/">
+                            <LogoText badge="Beta" />
+                        </a>
+                        <div
+                            className={"navbar-burger burger " + toggleClass}
+                            onClick={this.toggleBurger}
+                        >
+                            <span />
+                            <span />
+                            <span />
+                        </div>
+                    </div>
+                    <div className={"navbar-menu " + toggleClass}>
+                        <div className="navbar-end">
+                            <a
+                                className="navbar-item"
+                                href="#about"
+                                onClick={this.toggleBurger}
+                            >
+                                ABOUT
+                            </a>
+                            <a
+                                className="navbar-item"
+                                href="#roadMap"
+                                onClick={this.toggleBurger}
+                            >
+                                ROADMAP
+                            </a>
+                            <a
+                                className="navbar-item"
+                                href="#team"
+                                onClick={this.toggleBurger}
+                            >
+                                TEAM
+                            </a>
+                            <a
+                                className="navbar-item"
+                                href="#faq"
+                                onClick={this.toggleBurger}
+                            >
+                                FAQ
+                            </a>
+                            <a
+                                className="navbar-item"
+                                href="https://s3.amazonaws.com/skitbay-app/Skitbay_whitepaper_beta_1.0.pdf"
+                                target="_blank"
+                                onClick={this.toggleBurger}
+                            >
+                                WHITEPAPER
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
